325dfe22fa0f375b6897b159ff5efc21742ed6a6
sachushaji <sachushajiabraham@gmail.com>
2020-07-22 03:19:20 +0530

    Test for db

--- tests/db.test.js
+++ tests/db.test.js
@@ -34,3 +34,52 @@ describe('A new user should be added to the database', () => {
 
 
 });
+
+
+describe('Updates the index of a specifiable User', () => {
+  it('Should update the account object from the database that contains the specifed user', async () => {
+    await db.update(testData.accounts[0].twitter_id)
+    let response = await db.read(testData.accounts[0].twitter_id)
+    expect(response.address_index).equal(1);
+  });
+
+  // it('Should throw error Otherwise', async () => {
+  //   expect( db.update("")).to.throw();
+  // });
+});
+
+describe('A user should be removed from the database', () => {
+  it('Should remove the account object from the database that contains the specifed user', async () => {
+    await db.remove(testData.accounts[0].twitter_id)
+    let response = await db.read(testData.accounts[0].twitter_id)
+    expect(response).equal(null);
+    // expect(response.address_index).equal(0)
+  });
+
+  // it('Should throw error if the database does not have the specifed user', async () => {
+  //   // await db.remove("abcdepqrst");
+  //   // let response = await db.read("abcdepqrst");
+  //   // expect(response).be.false;
+  //   expect(db.remove("abcdepqrst")).to.throw(/Does not exist/);
+  //   // expect(response.address_index).equal(0)
+  // });
+});
+
+
+// describe('Returns the details of the specifed User', () => {
+//   it('Should return the account object from the database that contains the specifed user', async () => {
+//    let response = await db.read(testData.accounts[0].twitter_id);
+//    console.log("Response===", response);
+//     expect(response.address_index).equal(0);
+//     expect(response.helix_seed).equal(testData.accounts[0].helix_seed);
+//     expect(response.twitter_id).equal(testData.accounts[0].testData.accounts[0].twitter_id);
+
+//   });
+
+//   it('Should throw error Otherwise', async () => {
+//     expect( db.update(testData.accounts[0].twitter_id)).to.throw();
+//   });
+// });
+
+
+
